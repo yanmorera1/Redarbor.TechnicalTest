d408147c565ea4a75169709399032748b51192ff
Yan Carlos Morera <yanmorera1@gmail.com>
2026-01-28 17:47:23 -0500

    feat (get-employee-by-id): Add GetEmployeeById endpoint and Query - Add GetEmployeeById endpoint using Carter - Add GetEmployeeDto for queryable props - Add GetEmployeeByIdQuery and GetEmployeeByIdHandler using IEmployeeRepository hibrid repo for Dapper transactions - Add EmployeeExtensions for mapping extesions

---
+++ src/Redarbor.TechnicalTest.Application/Dtos/GetEmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿using Redarbor.TechnicalTest.Domain.Enums;
+
+namespace Redarbor.TechnicalTest.Application.Dtos;
+
+public record GetEmployeeDto(
+        int? Id,
+        int CompanyId,
+        string Email,
+        string? Fax,
+        string? Name,
+        int PortalId,
+        int RoleId,
+        EmployeeStatus StatusId,
+        string? Telephone,
+        string Username
+    );
---
+++ src/Redarbor.TechnicalTest.Application/Employees/Queries/GetEmployeeById/GetEmployeeByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Redarbor.TechnicalTest.Application.Employees.Queries.GetEmployeeById;
+
+internal class GetEmployeeByIdHandler
+    (IEmployeeRepository employeeRepository)
+    : IQueryHandler<GetEmployeeByIdQuery, GetEmployeeByIdResult>
+{
+    public async Task<GetEmployeeByIdResult> Handle(GetEmployeeByIdQuery query, CancellationToken cancellationToken)
+    {
+        Employee employee = await employeeRepository.GetByIdAsync(query.Id);
+
+        if (employee is null)
+        {
+            throw new EmployeeNotFoundException(query.Id);
+        }
+
+        return new GetEmployeeByIdResult(employee.ToDto());
+    }
+}
